d2ac9cb63ec88cdd7038e9f2f47cf12190780f19
Joey Schrader <joedanproperty@gmail.com>
2018-03-07 16:39:42 -0700

    added prettier to atom packages and got user backend auth working

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/change-me');
+mongoose.connect('mongodb://localhost/auth-boilerplate');
 
 const app = express();
+
+const auth = require('./routes/auth');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, './client/build')));
+
+//SET UP EXPRESS SESSION
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: false,
+    secure: false
+  }
+}));
+
+//SETUP PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//AUTH ROUTES
+app.use('/api/auth', auth)
 
 app.get('*', (request, response) => {
    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const App = () => (
-  <p>
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
+	<p>
+		To get started, edit <code>src/App.js</code> and save to reload.
+	</p>
 );
 
 export default App;
